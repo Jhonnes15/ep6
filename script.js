a05415ae7c913eeece31b748fdd6f891158d7dde
Jhonnes15 <128335679+Jhonnes15@users.noreply.github.com>
2023-10-10 11:31:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function tarefas(){
+    var bah = prompt("Escreva seu nome");
+    if(bah == "Jhonnes"){
+       alert("Bem vindo as tarefas de matematica do 1 ano do ensino médio Jhonnes.");
+
+       document.write("<p><h1>Bem vindo Jhonnes.<br></h1><h2>Essas são suas atividades:</h2></p>")
+}
+
+    if(bah == "Luigi"){
+        alert("Bem vindo as tarefas de matematica do 1 ano do ensino médio Luigi.");
+
+        document.write("<p><h1>Bem vindo Luigi.</h1><br><h2>Essas são suas atividades:</h2></p>")
+
+    }
+}
