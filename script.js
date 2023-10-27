d3a7c5438feb02af323c2f64472fbb37fc3bb7c8
Jhonnes15 <128335679+Jhonnes15@users.noreply.github.com>
2023-10-27 12:12:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -22,10 +22,12 @@ function funcao_resposta(){
     var resp8 = document.querySelector('input[name="questao8"]:checked');
     var resp9 = document.querySelector('input[name="questao9"]:checked');
     var resp10 = document.querySelector('input[name="questao10"]:checked');
+    var contador = 0;
 
     if(resp1 && resp2 && resp3 && resp4 && resp5 && resp6 && resp7 && resp8 && resp9 && resp10){
         if(resp1.value == "correto"){
             document.getElementById("resp_correta_1").innerHTML = "correta!";
+            contador = contador + 1;
         }
         else{
             document.getElementById("resp_errada_1").innerHTML = "errou burro!";
@@ -33,6 +35,7 @@ function funcao_resposta(){
        
         if(resp2.value == "correto"){
             document.getElementById("resp_correta_2").innerHTML = "correta!";
+            contador = contador + 1;
         }
         else{
             document.getElementById("resp_errada_2").innerHTML = "errou burro!";
@@ -40,6 +43,7 @@ function funcao_resposta(){
 
         if(resp3.value == "correto"){
             document.getElementById("resp_correta_3").innerHTML = "correta!";
+            contador = contador + 1;
         }
         else{
             document.getElementById("resp_errada_3").innerHTML = "errou burro!";
@@ -47,6 +51,7 @@ function funcao_resposta(){
 
         if(resp4.value == "correto"){
             document.getElementById("resp_correta_4").innerHTML = "correta!";
+            contador = contador + 1;
         }
         else{
             document.getElementById("resp_errada_4").innerHTML = "errou burro!";
@@ -54,6 +59,7 @@ function funcao_resposta(){
 
         if(resp5.value == "correto"){
             document.getElementById("resp_correta_5").innerHTML = "correta!";
+            contador = contador + 1;
         }
         else{
             document.getElementById("resp_errada_5").innerHTML = "errou burro!";
@@ -61,6 +67,7 @@ function funcao_resposta(){
 
         if(resp6.value == "correto"){
             document.getElementById("resp_correta_6").innerHTML = "correta!";
+            contador = contador + 1;
         }
         else{
             document.getElementById("resp_errada_6").innerHTML = "errou burro!";
@@ -68,6 +75,7 @@ function funcao_resposta(){
        
         if(resp7.value == "correto"){
             document.getElementById("resp_correta_7").innerHTML = "correta!";
+            contador = contador + 1;
         }
         else{
             document.getElementById("resp_errada_8").innerHTML = "errou burro!";
@@ -75,6 +83,7 @@ function funcao_resposta(){
 
         if(resp8.value == "correto"){
             document.getElementById("resp_correta_8").innerHTML = "correta!";
+            contador = contador + 1;
         }
         else{
             document.getElementById("resp_errada_8").innerHTML = "errou burro!";
@@ -82,6 +91,7 @@ function funcao_resposta(){
 
         if(resp9.value == "correto"){
             document.getElementById("resp_correta_9").innerHTML = "correta!";
+            contador = contador + 1;
         }
         else{
             document.getElementById("resp_errada_9").innerHTML = "errou burro!";
@@ -89,12 +99,24 @@ function funcao_resposta(){
 
         if(resp10.value == "correto"){
             document.getElementById("resp_correta_10").innerHTML = "correta!";
+            contador = contador + 1;
         }
         else{
             document.getElementById("resp_errada_10").innerHTML = "errou burro!";
         }
+        document.getElementById("quantos_acertos").innerHTML = "vc acertou " +contador+ " questoes";
+
     }
     else{
         alert("nao deixe questoes em branco!");
     }
+
+    function questionario(){
+       
+    }
+    
 }
+
+
+
+
